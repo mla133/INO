d5a0e9116d0f56bcd219fc3afeba5802d7505486
Matt Allen <matthew.allen@fmcti.com>
2016-06-03 09:45:33 -0400

    Removing notepad project (wasn't working with INO very well)

--- projects/notepad/.build/uno/Adafruit_TFTLCD/examples/tftpaint_shield/glcdfont.d
+++
@@ -1,1 +0,0 @@
-.build/uno/Adafruit_TFTLCD/examples/tftpaint_shield/glcdfont.d .build/uno/Adafruit_TFTLCD/examples/tftpaint_shield/glcdfont.o: lib/Adafruit_TFTLCD/examples/tftpaint_shield/glcdfont.c
